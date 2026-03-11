ce3eb427099318ea8395d500032638b3a3566fab
Divya Shedge <86007816+Div15@users.noreply.github.com>
2026-03-11 21:50:34 +0530

    Update firstProgram.java

--- firstProgram.java
+++ firstProgram.java
@@ -4,7 +4,7 @@ public class firstProgram {
     public static void main(String[] args)
     {
         // Prints "Hello, World" to the trminal window.
-        System.out.println("Hell, WorlD");
+        System.out.println("Hell, WrlD");
         System.out.println("Adding chans.");
     }
 }
